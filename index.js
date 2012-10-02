9a95d022fd9f3b50490ea220f7bfd2eccc063ca6
James Gosnell <jamesgosnell@gmail.com>
2012-10-01 23:37:56 -0500

    ADDED hires time tests(CPP code won)

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var worldjs = require('./hello');
 var worldcc = require('./build/Release/hello');
 
-console.log('js: hello '+worldjs.hello());
+var l = process.hrtime();
+worldjs.hello();
+l = process.hrtime(l);
 
-console.log('cp: hello '+worldcc.hello());
+console.log('Hello '+worldjs.hello());
+console.log('js: '+l[0]+' seconds and '+l[1]+' nanoseconds');
+
+var m = process.hrtime();
+worldcc.hello();
+m = process.hrtime(m);
+
+console.log('Hello '+worldcc.hello());
+console.log('cc: '+m[0]+' seconds and '+m[1]+' nanoseconds');
